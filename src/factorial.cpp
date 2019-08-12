9ad3971140b3363bff016122aaa52828d42ce3ae
Ivan Murashko <ivan.murashko@gmail.com>
2019-08-12 14:34:59 +0300

    Factorial in imperative style was added

--- src/factorial.cpp
+++ src/factorial.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int f(int n) {
+  if (n < 0)
+    throw std::invalid_argument("the argument has to be greater or equal 0");
   int res = 1;
   for (int i = 1; i <= n; ++i) {
     res *= i;
@@ -10,5 +12,6 @@ int f(int n) {
 
 int main(){
   std::cout << f(10) << std::endl;
+  std::cout << f(-10) << std::endl;
   return 0;
 }
